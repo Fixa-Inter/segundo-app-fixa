3b3499b71ab6235bb1ff2797cefca3509d55c856
raphaxnz <raphamendesy@gmail.com>
2026-08-17 19:53:58 -0300

    feat: adiciona XML de Login e importa fonte Poppins

--- app/src/main/java/com/aula/fixa/ui/auth/LoginActivity.java
+++ app/src/main/java/com/aula/fixa/ui/auth/LoginActivity.java
@@ -1,9 +1,6 @@
-package com.aula.fixa.ui.splash;
+package com.aula.fixa.ui.auth;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,28 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.aula.fixa.R;
-import com.aula.fixa.ui.auth.LoginActivity;
 
-public class SplashActivity extends AppCompatActivity {
-
-    private static final long SPLASH_DURATION_MILLIS = 3000;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }, SPLASH_DURATION_MILLIS);
     }
 }
